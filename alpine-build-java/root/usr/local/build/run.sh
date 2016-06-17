dde66a4718afcd8641aa3185d8825350cce0f35f
Boris HUISGEN <bhuisgen@hbis.fr>
2016-06-17 11:05:36 +0200

    Add support custom git branch & ref
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- alpine-build-java/root/usr/local/build/run.sh
+++ alpine-build-java/root/usr/local/build/run.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
-# By default this script does nothing.
+set -e
+
+if [ -z ${BUILD_REPOSITORY} ]; then
+    echo "BUILD_REPOSITORY is not set, aborting"
+
+    exit 1
+fi
+
+if [ -z ${BUILD_REFNAME} ]; then
+    echo "BUILD_REFNAME is not set, aborting"
+
+    exit 1
+fi
+
+if [ -z ${BUILD_REF} ]; then
+    echo "BUILD_REF is not set, aborting"
+
+    exit 1
+fi
+
+git clone ${BUILD_REPOSITORY} -b ${BUILD_REFNAME} --progress project
+cd project
+git checkout ${BUILD_REF}
+
+mvn package -B
 
 exit 0
