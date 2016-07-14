da10c7254b8e4d4293f7bca293908543a0ff9568
Boris HUISGEN <bhuisgen@hbis.fr>
2016-07-15 00:33:28 +0200

    Add configure script and make install support
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- alpine-build-make/root/usr/local/build/job.sh
+++ alpine-build-make/root/usr/local/build/job.sh
@@ -20,4 +20,22 @@ if [ ! -z ${PROJECT_SUBDIR} ]; then
     cd ${PROJECT_SUBDIR}
 fi
 
+if [ -z ${CONFIGURE} ]; then
+    CONFIGURE=1
+fi
+
+if [ -z ${MAKE_INSTALL} ]; then
+    MAKE_INSTALL=1
+fi
+
+if [ ! -z ${CONFIGURE} ] && [ ${CONFIGURE} -eq 1 ]; then
+    if [ -x configure ]; then
+        ./configure ${CONFIGURE_OPTIONS}
+    fi
+fi
+
 make ${MAKE_OPTIONS} ${MAKE_TARGET} ${MAKE_TARGETOPTIONS}
+
+if [ ! -z ${MAKE_INSTALL} ] && [ ${MAKE_INSTALL} -eq 1 ]; then
+    make ${MAKE_OPTIONS} install ${MAKE_INSTALLOPTIONS}
+fi
