558581a39c759f3b1e6fbcd20187d1c727418dd4
Boris HUISGEN <bhuisgen@hbis.fr>
2018-02-28 11:27:43 +0100

    Fix config for local tar restore
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- alpine-backuppc/rootfs/etc/BackupPC/pc/config.pl
+++ alpine-backuppc/rootfs/etc/BackupPC/pc/config.pl
@@ -1,5 +1,7 @@
+$Conf{ClientNameAlias} = 'localhost';
 $Conf{XferMethod} = 'tar';
 $Conf{TarShareName} = [
   '/etc/BackupPC'
 ];
 $Conf{TarClientCmd} = '/usr/bin/sudo $tarPath -c -v -f - -C $shareName+ --totals';
+$Conf{TarClientRestoreCmd} = '/usr/bin/sudo $tarPath -x -p --numeric-owner --same-owner -v -f - -C $shareName+';
