f851b2531cf69ba4d12426be9d50cab64fd4b752
Boris HUISGEN <bhuisgen@hbis.fr>
2018-03-06 22:01:53 +0100

    Check config before running backup
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- alpine-backup-mongodb/rootfs/usr/local/backup/backup.sh
+++ alpine-backup-mongodb/rootfs/usr/local/backup/backup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+if [ -z "$BACKUP_MONGODB_HOST" ] || [ -z "$BACKUP_MONGODB_PORT" ] || [ -z "$BACKUP_MONGODB_DATABASE" ]; then
+    exit 1
+fi
+
 trap 'exit 2' ERR INT TERM
 
 [ -z $BACKUP_PATH ] && BACKUP_PATH="/var/backups"
-[ -z $BACKUP_MONGODB_DATABASE ] && exit 1
 
 BACKUP_DIR="$BACKUP_PATH/$(date +%Y%m%d_%H%M%S)"
 
-mkdir -p "$BACKUP_DIR" || exit 1
+mkdir -p "$BACKUP_DIR" || exit 3
 
 databases="${BACKUP_MONGODB_DATABASE//,/ }"
 
