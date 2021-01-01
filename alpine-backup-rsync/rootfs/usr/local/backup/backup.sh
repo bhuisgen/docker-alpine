f33b0f1a628c934fb1e86ce45c7d01b9fd9ed202
Boris HUISGEN <bhuisgen@hbis.fr>
2021-01-01 08:20:22 +0100

    Update alpine-backup-rsync image

--- alpine-backup-rsync/rootfs/usr/local/backup/backup.sh
+++ alpine-backup-rsync/rootfs/usr/local/backup/backup.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -z "$BACKUP_RSYNC_DIR" ]; then
-    exit 1
-fi
-
 trap 'exit 2' ERR INT TERM
 
-[ -z "$BACKUP_PATH" ] && BACKUP_PATH="/var/backups"
+[ -z "${BACKUP_PATH}" ] && BACKUP_PATH="/var/backups"
+[ -z "${BACKUP_DATA}" ] && BACKUP_DATA="/data"
+[ -z "${BACKUP_ROTATE}" ] && BACKUP_ROTATE=1
+[ -z "${BACKUP_ROTATE_DAYS}" ] && BACKUP_ROTATE_DAYS=4
 
-BACKUP_DIR="$BACKUP_PATH/$(date +%Y%m%d_%H%M%S)"
+backup_dir="${BACKUP_PATH}/$(date +%Y%m%d_%H%M%S)"
 
-mkdir -p "$BACKUP_DIR" || exit 3
+mkdir -p "${backup_dir}"
 
-rsync -a "$BACKUP_RSYNC_DIR/" "$BACKUP_DIR/"
+rsync -a "${BACKUP_DATA}/" "${backup_dir}/"
+
+if [ ${BACKUP_ROTATE} -eq 1 ]; then
+    find "${BACKUP_PATH}" -mindepth 1 -maxdepth 1 -type d -mtime "+${BACKUP_ROTATE_DAYS}" -print0 |
+        xargs -0 -I {} /bin/rm -rf "{}"
+fi
