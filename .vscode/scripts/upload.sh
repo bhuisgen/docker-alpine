8a1f5e7bdcf4457e5895c1286524705ef70d8b96
Boris HUISGEN <bhuisgen@hbis.fr>
2022-11-09 09:39:15 +0000

    Remove project files

--- .vscode/scripts/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-DOCKER_VOLUME=devcontainer_$(basename $(pwd))
-
-id=$(docker run -d --rm -v ${DOCKER_VOLUME}:/data alpine:3.15 /bin/sh -c 'sleep 3600')
-docker cp ./ ${id}:/data/.
-docker kill ${id} >/dev/null 2>/dev/null
